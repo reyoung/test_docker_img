d7b06c664c8f4e19df23c8517b52ba6d5e54e7b2
Yu Yang <yuyang18@baidu.com>
2016-08-30 23:23:24 +0800

    Do generate

--- gpu/build.sh
+++ gpu/build.sh
@@ -12,22 +12,24 @@ apt-get update
 apt-get install -y cmake libprotobuf-dev protobuf-compiler git \
     libgoogle-glog-dev libgflags-dev libatlas-dev libatlas3-base g++ m4 python-pip\
     python-protobuf python-numpy python-dev swig
+
+if [ ${WITH_GPU} == 'ON' ]; then
+  ln -s /usr/lib/x86_64-linux-gnu/libcudnn.so /usr/lib/libcudnn.so
+fi
+
+
 cd ~
 git clone https://github.com/baidu/Paddle.git paddle
 cd paddle
 mkdir build
 cd build
-cmake .. -DWITH_DOC=OFF -DWITH_GPU=${WITH_GPU} -DWITH_SWIG_PY=ON
+cmake .. -DWITH_DOC=OFF -DWITH_GPU=${WITH_GPU} -DWITH_SWIG_PY=ON -DCUDNN_ROOT=/usr/
 make -j `nproc`
 # because durning make install, there are several warning, so set +e, do not cause abort
 make install
 echo 'export LD_LIBRARY_PATH=/usr/lib64:${LD_LIBRARY_PATH}' >> /etc/profile
 pip ${PIP_GENERAL_ARGS} install ${PIP_INSTALL_ARGS} /usr/local/opt/paddle/share/wheels/*.whl
 paddle version  # print version after build
-
-if [ ${WITH_GPU} == 'ON' ]; then
-  ln -s /usr/lib/x86_64-linux-gnu/libcudnn.so /usr/lib/libcudnn.so
-fi
 
 if [ ${WITH_DEMO} == "ON" ]; then
   apt-get install -y wget unzip perl python-matplotlib tar xz-utils bzip2 gzip coreutils\
